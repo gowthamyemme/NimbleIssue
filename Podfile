e13ea3f8c7551c245bb8b9d37f8ef40e1f28a67c
Gowtham Yemme <gyemme@paypal.com>
2015-02-06 16:16:25 -0800

    Initialized a new git repo using pods

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+
+target 'iOSUnitTestingTests', :exclusive => true do
+  pod 'Quick'
+  pod 'Nimble'
+end
+
---
+++ Pods/Target Support Files/Pods-iOSUnitTestingTests-Quick/Pods-iOSUnitTestingTests-Quick-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSUnitTestingTests_Quick : NSObject
+@end
+@implementation PodsDummy_Pods_iOSUnitTestingTests_Quick
+@end
